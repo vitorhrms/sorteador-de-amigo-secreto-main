bc27b6dc3f66cbbd0ed48fc6e648d101e532d1d7
Vitor Henrique Ramos <vitor.ramos@nm.local>
2023-10-24 14:54:48 -0300

    first commit

---
+++ src/components/Formulario.tsx
@@ -0,0 +1,10 @@
+const Formulario = () => {
+  return (
+    <form>
+      <input type="text" placeholder="Insira os nomes dos participante" />
+      <button disabled>Adicionar</button>
+    </form>
+  );
+};
+
+export default Formulario;
